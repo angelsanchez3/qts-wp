a356645a6a0ff027e1665815b1d6ba69c3912c45
Angel Sanchez <angelsanmag@gmail.com>
2022-07-15 11:38:33 +0100

    Adds header.php and footer.php and links them to index.php

---
+++ footer.php
@@ -0,0 +1,240 @@
+<footer>
+    <div class="realfactory-footer-wrapper">
+        <div class="realfactory-footer-container realfactory-container clearfix">
+            <div class="realfactory-footer-column realfactory-item-pdlr realfactory-column-15">
+                <div id="text-3" class="widget widget_text realfactory-widget">
+                    <div class="textwidget c-footer-logo">
+                        <span class="gdlr-core-space-shortcode" style="margin-top: -2px;"></span>
+                        <img src="images/qts-white-jpg.jpg" alt="" width="110px">
+                        <span class="gdlr-core-space-shortcode" style="margin-top: 7px;"></span>
+                        <div class="realfactory-top-bar-right-social">
+                            <a href="#" target="_blank" class="facebook-footer" title="facebook"><i class="fa fa-facebook"></i></a>
+                            <a href="#" target="_blank" class="linkedin-footer" title="linkedin"><i class="fa fa-linkedin"></i></a>
+                            <a href="#" target="_blank" class="youtube-footer" title="youtube"><i class="fa fa-youtube-play"></i></a>
+                        </div>
+                        <div class="c-newsletter">
+                            <div class="c-newsletter-text">Subscreva à nossa newsletter</div>
+                            <form>
+                                <input type="text" name="your-company" value="" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Email">
+                                <button type="submit" class="submit-button"><span><em>Subscrever</em></span></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="realfactory-footer-column realfactory-item-pdlr realfactory-column-15">
+                <div id="nav_menu-4" class="widget widget_nav_menu realfactory-widget">
+                    <h3 class="realfactory-widget-title">A Empresa</h3>
+                    <div class="menu-market-sectors-container">
+                        <ul id="menu-market-sectors" class="menu">
+                            <li class="menu-item"><a href="about.html">Sobre Nós</a></li>
+                            <li class="menu-item"><a href="gallery.html">Informações</a></li>
+                            <li class="menu-item"><a href="noticias.html">Notícias</a></li>
+                            <li class="menu-item"><a href="contacts.html">Contactos</a></li>
+                            <li class="menu-item"><a href="page.html">Política de Privacidade</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="realfactory-footer-column realfactory-item-pdlr realfactory-column-15">
+                <div id="recent-posts-3" class="widget widget_recent_entries realfactory-widget">
+                    <h3 class="realfactory-widget-title">Serviços</h3>
+                    <ul>
+                        <li> <a href="services.html">Serviço 1</a></li>
+                        <li> <a href="services.html">Serviço 2</a></li>
+                        <li> <a href="services.html">Serviço 3</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="realfactory-footer-column realfactory-item-pdlr realfactory-column-15">
+                <div id="text-7" class="widget widget_text realfactory-widget">
+                    <h3 class="realfactory-widget-title">Contactos</h3>
+                    <div class="textwidget">
+                        <p>
+                            <i class="fa fa-location-arrow" style="font-size: 20px;color: #99c329;margin-left: 0px;margin-right: 10px;"></i> R. Pomar de Marvila, Lote 15, Parque Industrial de Sequeira, Braga, Portugal
+                            <br> <span class="gdlr-core-space-shortcode" style="margin-top: -6px;"></span>
+                            <br> <i class="fa fa-phone" style="font-size: 20px;color: #99c329;margin-right: 10px;"></i> +351 911 509 738
+                            <br> <span class="gdlr-core-space-shortcode" style="margin-top: -6px;"></span>
+                            <br> <i class="fa fa-envelope-o" style="font-size: 20px;color: #99c329;margin-left: 0px;margin-right: 10px;"></i> geral@qtservices.pt
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+</div>
+</div>
+
+<!-- <script type='text/javascript' src='js/jquery/jquery.js'></script>
+    <script type='text/javascript' src='js/jquery/jquery-migrate.min.js'></script>
+    <script type='text/javascript' src='plugins/revslider/public/assets/js/jquery.themepunch.tools.min.js'></script>
+    <script type='text/javascript' src='plugins/revslider/public/assets/js/jquery.themepunch.revolution.min.js'></script>
+
+    <script type='text/javascript' src='js/jquery/ui/effect.min.js'></script>
+
+    <script type='text/javascript' src='js/script.js'></script>
+    <script type='text/javascript' src='plugins/goodlayers-core/plugins/combine/script.js'></script>
+
+    <script type='text/javascript' src='plugins/goodlayers-core/include/js/page-builder.js'></script>
+
+
+    <script type="text/javascript" src="plugins/revslider/public/assets/js/extensions/revolution.extension.slideanims.min.js"></script>
+    <script type="text/javascript" src="plugins/revslider/public/assets/js/extensions/revolution.extension.layeranimation.min.js"></script>
+    <script type="text/javascript" src="plugins/revslider/public/assets/js/extensions/revolution.extension.kenburn.min.js"></script>
+    <script type="text/javascript" src="plugins/revslider/public/assets/js/extensions/revolution.extension.navigation.min.js"></script>
+    <script type="text/javascript" src="plugins/revslider/public/assets/js/extensions/revolution.extension.parallax.min.js"></script>  
+    <script type="text/javascript" src="plugins/revslider/public/assets/js/extensions/revolution.extension.actions.min.js"></script> 
+    <script type="text/javascript">
+        /*<![CDATA[*/
+        function setREVStartSize(e) {
+            try {
+                var i = jQuery(window).width(),
+                    t = 9999,
+                    r = 0,
+                    n = 0,
+                    l = 0,
+                    f = 0,
+                    s = 0,
+                    h = 0;
+                if (e.responsiveLevels && (jQuery.each(e.responsiveLevels, function(e, f) {
+                        f > i && (t = r = f, l = e), i > f && f > r && (r = f, n = e)
+                    }), t > r && (l = n)), f = e.gridheight[l] || e.gridheight[0] || e.gridheight, s = e.gridwidth[l] || e.gridwidth[0] || e.gridwidth, h = i / s, h = h > 1 ? 1 : h, f = Math.round(h * f), "fullscreen" == e.sliderLayout) {
+                    var u = (e.c.width(), jQuery(window).height());
+                    if (void 0 != e.fullScreenOffsetContainer) {
+                        var c = e.fullScreenOffsetContainer.split(",");
+                        if (c) jQuery.each(c, function(e, i) {
+                            u = jQuery(i).length > 0 ? u - jQuery(i).outerHeight(!0) : u
+                        }), e.fullScreenOffset.split("%").length > 1 && void 0 != e.fullScreenOffset && e.fullScreenOffset.length > 0 ? u -= jQuery(window).height() * parseInt(e.fullScreenOffset, 0) / 100 : void 0 != e.fullScreenOffset && e.fullScreenOffset.length > 0 && (u -= parseInt(e.fullScreenOffset, 0))
+                    }
+                    f = u
+                } else void 0 != e.minHeight && f < e.minHeight && (f = e.minHeight);
+                e.c.closest(".rev_slider_wrapper").css({
+                    height: f
+                })
+            } catch (d) {
+                console.log("Failure at Presize of Slider:" + d)
+            }
+        }; /*]]>*/
+    </script>
+    <script type="text/javascript">
+        /*<![CDATA[*/
+        function revslider_showDoubleJqueryError(sliderID) {
+            var errorMessage = "Revolution Slider Error: You have some jquery.js library include that comes after the revolution files js include.";
+            errorMessage += "<br> This includes make eliminates the revolution slider libraries, and make it not work.";
+            errorMessage += "<br><br> To fix it you can:<br>&nbsp;&nbsp;&nbsp; 1. In the Slider Settings -> Troubleshooting set option:  <strong><b>Put JS Includes To Body</b></strong> option to true.";
+            errorMessage += "<br>&nbsp;&nbsp;&nbsp; 2. Find the double jquery.js include and remove it.";
+            errorMessage = "<span style='font-size:16px;color:#BC0C06;'>" + errorMessage + "</span>";
+            jQuery(sliderID).show().html(errorMessage);
+        } /*]]>*/
+    </script>
+    <script>
+        /*<![CDATA[*/
+        var htmlDiv = document.getElementById("rs-plugin-settings-inline-css");
+        var htmlDivCss = "";
+        if (htmlDiv) {
+            htmlDiv.innerHTML = htmlDiv.innerHTML + htmlDivCss;
+        } else {
+            var htmlDiv = document.createElement("div");
+            htmlDiv.innerHTML = "<style>" + htmlDivCss + "</style>";
+            document.getElementsByTagName("head")[0].appendChild(htmlDiv.childNodes[0]);
+        } /*]]>*/
+    </script>
+    <script>
+        /*<![CDATA[*/
+        var htmlDiv = document.getElementById("rs-plugin-settings-inline-css");
+        var htmlDivCss = "";
+        if (htmlDiv) {
+            htmlDiv.innerHTML = htmlDiv.innerHTML + htmlDivCss;
+        } else {
+            var htmlDiv = document.createElement("div");
+            htmlDiv.innerHTML = "<style>" + htmlDivCss + "</style>";
+            document.getElementsByTagName("head")[0].appendChild(htmlDiv.childNodes[0]);
+        } /*]]>*/
+    </script>
+    <script type="text/javascript">
+        setREVStartSize({
+            c: jQuery('#rev_slider_1_1'),
+            gridwidth: [1180],
+            gridheight: [725],
+            sliderLayout: 'auto'
+        });
+        var revapi1, tpj = jQuery;
+        tpj(document).ready(function() {
+            if (tpj("#rev_slider_1_1").revolution == undefined) {
+                revslider_showDoubleJqueryError("#rev_slider_1_1");
+            } else {
+                revapi1 = tpj("#rev_slider_1_1").show().revolution({
+                    sliderType: "standard",
+                    jsFileLocation: "//demo.goodlayers.com/realfactory/wp-content/plugins/revslider/public/assets/js/",
+                    sliderLayout: "auto",
+                    dottedOverlay: "none",
+                    delay: 9000,
+                    navigation: {
+                        keyboardNavigation: "off",
+                        keyboard_direction: "horizontal",
+                        mouseScrollNavigation: "off",
+                        mouseScrollReverse: "default",
+                        onHoverStop: "off",
+                        arrows: {
+                            style: "uranus",
+                            enable: true,
+                            hide_onmobile: false,
+                            hide_onleave: false,
+                            tmp: '',
+                            left: {
+                                h_align: "left",
+                                v_align: "center",
+                                h_offset: 20,
+                                v_offset: 0
+                            },
+                            right: {
+                                h_align: "right",
+                                v_align: "center",
+                                h_offset: 20,
+                                v_offset: 0
+                            }
+                        }
+                    },
+                    visibilityLevels: [1240, 1024, 778, 480],
+                    gridwidth: 1180,
+                    gridheight: 725,
+                    lazyType: "none",
+                    shadow: 0,
+                    spinner: "off",
+                    stopLoop: "off",
+                    stopAfterLoops: -1,
+                    stopAtSlide: -1,
+                    shuffle: "off",
+                    autoHeight: "off",
+                    disableProgressBar: "on",
+                    hideThumbsOnMobile: "off",
+                    hideSliderAtLimit: 0,
+                    hideCaptionAtLimit: 0,
+                    hideAllCaptionAtLilmit: 0,
+                    debugMode: false,
+                    fallbacks: {
+                        simplifyAll: "off",
+                        nextSlideOnWindowFocus: "off",
+                        disableFocusListener: false,
+                    }
+                });
+            }
+        });
+    </script>
+    <script>
+        /*<![CDATA[*/
+        var htmlDivCss = unescape("%23rev_slider_1_1%20.uranus.tparrows%20%7B%0A%20%20width%3A50px%3B%0A%20%20height%3A50px%3B%0A%20%20background%3Argba%28255%2C255%2C255%2C0%29%3B%0A%20%7D%0A%20%23rev_slider_1_1%20.uranus.tparrows%3Abefore%20%7B%0A%20width%3A50px%3B%0A%20height%3A50px%3B%0A%20line-height%3A50px%3B%0A%20font-size%3A40px%3B%0A%20transition%3Aall%200.3s%3B%0A-webkit-transition%3Aall%200.3s%3B%0A%20%7D%0A%20%0A%20%20%23rev_slider_1_1%20.uranus.tparrows%3Ahover%3Abefore%20%7B%0A%20%20%20%20opacity%3A0.75%3B%0A%20%20%7D%0A");
+        var htmlDiv = document.getElementById('rs-plugin-settings-inline-css');
+        if (htmlDiv) {
+            htmlDiv.innerHTML = htmlDiv.innerHTML + htmlDivCss;
+        } else {
+            var htmlDiv = document.createElement('div');
+            htmlDiv.innerHTML = '<style>' + htmlDivCss + '</style>';
+            document.getElementsByTagName('head')[0].appendChild(htmlDiv.childNodes[0]);
+        } /*]]>*/
+    </script> -->
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en-US" class="no-js">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>QTS Quality, Trading & Services</title>
+
+
+    <!-- <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Hind%3A300%2Cregular%2C500%2C600%2C700&#038;subset=latin%2Clatin-ext%2Cdevanagari&#038;ver=4.9.7' type='text/css' media='all'>
+
+    <link rel='stylesheet' href='plugins/revslider/public/assets/css/settings.css' type='text/css' media='all'>
+    <link rel='stylesheet' href='css/style-core.css' type='text/css' media='all'>
+    <link rel='stylesheet' href='css/rftr-style-custom.css' type='text/css' media='all'>
+    <link rel='stylesheet' href='plugins/goodlayers-core/plugins/combine/style.css' type='text/css' media='all'>
+    <link rel='stylesheet' href='plugins/goodlayers-core/include/css/page-builder.css' type='text/css' media='all'>
+
+
+    <link rel='stylesheet' href='css/custom.css' type='text/css' media='all'> -->
+
+</head>
+
+<body data-rsssl="1" class="home page-template-default page page-id-2039 gdlr-core-body woocommerce-no-js realfactory-body realfactory-body-front realfactory-full  realfactory-with-sticky-navigation gdlr-core-link-to-lightbox">
+    <div class="realfactory-mobile-header-wrap">
+        <div class="realfactory-mobile-header realfactory-header-background realfactory-style-slide" id="realfactory-mobile-header">
+            <div class="realfactory-mobile-header-container realfactory-container">
+                <div class="realfactory-logo  realfactory-item-pdlr">
+                    <div class="realfactory-logo-inner">
+                        <a href="index.html"><img src="images/qts-110x52.png" alt="QTS Logo"></a>
+                    </div>
+                </div>
+                <div class="realfactory-mobile-menu-right">
+                    <div class="realfactory-main-menu-search" id="realfactory-mobile-top-search"><i class="fa fa-search"></i></div>
+                    <div class="realfactory-top-search-wrap">
+                        <div class="realfactory-top-search-close"></div>
+                        <div class="realfactory-top-search-row">
+                            <div class="realfactory-top-search-cell">
+                                <form role="search" method="get" class="search-form" action="index.html">
+                                    <input type="text" class="search-field realfactory-title-font" placeholder="Pesquisar..." value="" name="s">
+                                    <div class="realfactory-top-search-submit"><i class="fa fa-search"></i></div>
+                                    <input type="submit" class="search-submit" value="Search">
+                                    <div class="realfactory-top-search-close"><i class="icon_close"></i></div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="realfactory-mobile-menu"><a class="realfactory-mm-menu-button realfactory-mobile-menu-button realfactory-mobile-button-hamburger-with-border" href="#realfactory-mobile-menu"><i class="fa fa-bars"></i></a>
+                        <div class="realfactory-mm-menu-wrap realfactory-navigation-font" id="realfactory-mobile-menu" data-slide="right">
+                            <ul id="menu-main-navigation" class="m-menu">
+                                <li class="menu-item menu-item-has-children"><a href="index.html">Home</a>
+                                </li>
+                                <li class="menu-item menu-item-has-children"><a href="about.html">Sobre Nós</a>
+                                </li>
+                                <li class="menu-item menu-item-has-children"><a href="#">Serviços</a>
+                                    <ul class="sub-menu">
+                                        <li class="menu-item"><a href="services.html">Serviço 1</a></li>
+                                        <li class="menu-item"><a href="services.html">Serviço 2</a></li>
+                                        <li class="menu-item"><a href="services.html">Serviço 3</a></li>
+                                    </ul>
+                                </li>
+                                <li class="menu-item menu-item-has-children"><a href="gallery.html">Informações</a>
+                                </li>
+                                <li class="menu-item menu-item-has-children"><a href="noticias.html">Notícias</a>
+                                </li>
+                                <li class="menu-item menu-item-has-children"><a href="contacts.html">Contactos</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="realfactory-body-outer-wrapper ">
+        <div class="realfactory-body-wrapper clearfix  realfactory-with-transparent-navigation realfactory-with-frame">
+            <div class="realfactory-top-bar">
+                <div class="realfactory-top-bar-background"></div>
+                <div class="realfactory-top-bar-container clearfix realfactory-container ">
+                    <div class="realfactory-top-bar-left realfactory-item-pdlr c-header-logo-wrapper">
+                        <div class="realfactory-top-bar-right-social c-header-logo">
+                            <a href="index.html"><img src="images/qts-110x52.png" alt="QTS Logo"></a>
+                        </div>
+                    </div>
+                    <div class="realfactory-top-bar-right realfactory-item-pdlr c-header-social-wrapper">
+                        <div class="realfactory-top-bar-right-social">
+                            <a href="#" target="_blank" class="realfactory-top-bar-social-icon" title="facebook"><i class="fa fa-facebook"></i></a>
+                            <a href="#" target="_blank" class="realfactory-top-bar-social-icon" title="linkedin"><i class="fa fa-linkedin"></i></a>
+                            <a href="#" target="_blank" class="realfactory-top-bar-social-icon" title="youtube"><i class="fa fa-youtube-play"></i></a>
+                        </div>
+                    </div>
+                    <div class="realfactory-top-bar-right realfactory-item-pdlr">
+                        <div class="gdlr-core-dropdown-tab gdlr-core-js clearfix">
+                            <div class="gdlr-core-dropdown-tab-title"><span class="gdlr-core-head">Português</span>
+                                <div class="gdlr-core-dropdown-tab-head-wrap">
+                                    <div class="gdlr-core-dropdown-tab-head gdlr-core-active" data-index="0">Português</div>
+                                    <div class="gdlr-core-dropdown-tab-head " data-index="1">English</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="realfactory-top-bar-right realfactory-item-pdlr">
+                        <div class="gdlr-core-dropdown-tab gdlr-core-js clearfix">
+                            <div class="gdlr-core-dropdown-tab-content-wrap">
+                                <div class="gdlr-core-dropdown-tab-content gdlr-core-active" data-index="0">
+                                    <i class="fa fa-phone" style="font-size: 16px;color: #99c329;margin-right: 10px;"></i> +351 911 509 738 <i class="fa fa-envelope-o" style="font-size: 16px;color: #99c329;margin-left: 24px;margin-right: 10px;"></i> geral@qtservices.pt
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="realfactory-navigation-bar-wrap  realfactory-style-transparent realfactory-sticky-navigation realfactory-sticky-navigation-height realfactory-style-left  realfactory-style-fixed realfactory-without-placeholder">
+                <div class="realfactory-navigation-background"></div>
+                <div class="realfactory-navigation-container clearfix  realfactory-container">
+                    <div class="realfactory-navigation realfactory-item-pdlr clearfix ">
+                        <div class="realfactory-main-menu" id="realfactory-main-menu">
+                            <ul id="menu-main-navigation-1" class="sf-menu">
+                                <li class="menu-item menu-item-home current-menu-item menu-item-has-children realfactory-normal-menu">
+                                    <a href="index.html" class="sf-with-ul-pre">Home</a>
+                                </li>
+                                <li class="menu-item menu-item-has-children realfactory-normal-menu">
+                                    <a href="about.html" class="sf-with-ul-pre">Sobre Nós</a>
+                                </li>
+                                <li class="menu-item menu-item-has-children realfactory-normal-menu">
+                                    <a href="#" class="sf-with-ul-pre">Serviços</a>
+                                    <ul class="sub-menu">
+                                        <li class="menu-item" data-size="60"><a href="services.html">Serviço 1</a></li>
+                                        <li class="menu-item" data-size="60"><a href="services.html">Serviço 2</a></li>
+                                        <li class="menu-item" data-size="60"><a href="services.html">Serviço 3</a></li>
+                                    </ul>
+                                </li>
+                                <li class="menu-item menu-item-has-children realfactory-mega-menu">
+                                    <a href="gallery.html" class="sf-with-ul-pre">Informações</a>
+                                </li>
+                                <li class="menu-item menu-item-has-children realfactory-normal-menu">
+                                    <a href="noticias.html" class="sf-with-ul-pre">Notícias</a>
+                                </li>
+                                <li class="menu-item menu-item-has-children realfactory-mega-menu">
+                                    <a href="contacts.html" class="sf-with-ul-pre">Contactos</a>
+                                </li>
+                            </ul>
+                            <div class="realfactory-navigation-slide-bar" id="realfactory-navigation-slide-bar"></div>
+                        </div>
+                        <div class="realfactory-main-menu-right-wrap clearfix ">
+                            <div class="realfactory-main-menu-search" id="realfactory-top-search"><i class="fa fa-search"></i></div>
+                            <div class="realfactory-top-search-wrap">
+                                <div class="realfactory-top-search-close"></div>
+                                <div class="realfactory-top-search-row">
+                                    <div class="realfactory-top-search-cell">
+                                        <form role="search" method="get" class="search-form" action="index.html">
+                                            <input type="text" class="search-field realfactory-title-font" placeholder="Pesquisar..." value="" name="s">
+                                            <div class="realfactory-top-search-submit"><i class="fa fa-search"></i></div>
+                                            <input type="submit" class="search-submit" value="Search">
+                                            <div class="realfactory-top-search-close"><i class="icon_close"></i></div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
