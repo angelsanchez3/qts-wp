5fa44c5e5adef03ada65be9645c3064865628962
Angel Sanchez <angelsanmag@gmail.com>
2022-07-21 15:17:40 +0100

    Adds 404 page

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<div class="realfactory-page-wrapper" id="realfactory-page-wrapper" style="padding-top: 60px;">
+    <div class="realfactory-not-found-wrap" id="realfactory-full-no-header-wrap">
+        <div class="realfactory-not-found-background"></div>
+        <div class="realfactory-not-found-container realfactory-container">
+            <div class="realfactory-not-found-content realfactory-item-pdlr">
+                <h1 class="realfactory-not-found-head">404</h1>
+                <h3 class="realfactory-not-found-title realfactory-content-font">Página Não Encontrada</h3>
+                <div class="realfactory-not-found-caption">Não foi possível localizar a página que procura.</div>
+                <div class="realfactory-not-found-back-to-home" style="padding-top: 10px;"><a href="<?php echo site_url(''); ?>">Voltar à Home</a></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
